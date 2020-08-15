fc0fe4a372427dd96f1e1507610d884bc77b066a
Justin Cichra <1342149+jrcichra@users.noreply.github.com>
2020-08-14 21:57:55 -0400

    try actual deployment

--- up.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-set -x
-sudo apt update && sudo apt install -y cpu-checker vagrant qemu-kvm libvirt-daemon-system libvirt-clients bridge-utils virtinst virt-manager
-kvm-ok
-cd k8s-vagrant-multi-node
-NODE_COUNT=5 BOX_OS=ubuntu VAGRANT_DEFAULT_PROVIDER=libvirt make up -j6
-vagrant global-status
